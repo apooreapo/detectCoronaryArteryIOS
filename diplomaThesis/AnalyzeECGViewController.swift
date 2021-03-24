c82d3fea65818a952a625309092e3d38fea94c9c
Apostolou Orestis <orestisapostolou@yahoo.gr>
2021-03-24 15:43:36 +0200

    Had some progress with analyzing ECG and creating a segue

---
+++ diplomaThesis/AnalyzeECGViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AnalyzeECGViewController.swift
+//  diplomaThesis
+//
+//  Created by User on 24/3/21.
+//
+
+import Foundation
---
+++ diplomaThesis/K.swift
@@ -0,0 +1,8 @@
+//
+//  K.swift
+//  diplomaThesis
+//
+//  Created by User on 24/3/21.
+//
+
+import Foundation
